724fc07988071469468d06886503b46f7db32ac7
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-09-23 18:31:44 -0600

    Started top.v

--- top.v
+++ top.v
@@ -1,1 +1,9 @@
-// Implement top level module
+module top(
+  input [6:0] sw,
+  output [1:0] led
+)
+  //TODO: feed inputs into circuitA
+  //TODO: feed inputs into circuitB
+  //find out how to add wires
+
+endmodule
