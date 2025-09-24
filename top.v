3cbf49272151704ab3c9ee3013b1110c862354f6
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-09-24 14:15:34 -0600

    Top.v finished

--- top.v
+++ top.v
@@ -1,12 +1,15 @@
 module top(
   input [6:0] sw,
   output [1:0] led
-)
+);
 
   wire w1;
   
-  //TODO: feed inputs into circuit
-  //TODO: feed inputs into circuitB
-  //find out how to add wires
+  //circuitA
+  circuit_a a(.A(sw[0]),.B(sw[1]),.C(sw[2]),.D(sw[3]),.Y(w1));
+  assign led[0] = w1;
+  
+  //circuitB
+  circuit_b b(.A(w1), .B(sw[4]), .C(sw[5]), .D(sw[6]), .Y(led[1]));
 
 endmodule
