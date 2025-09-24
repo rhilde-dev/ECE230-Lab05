36d842ffb9215afce47296d61e45948be7c9c834
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-09-24 13:49:41 -0600

    Update top.v

--- top.v
+++ top.v
@@ -2,7 +2,10 @@ module top(
   input [6:0] sw,
   output [1:0] led
 )
-  //TODO: feed inputs into circuitA
+
+  wire w1;
+  
+  //TODO: feed inputs into circuit
   //TODO: feed inputs into circuitB
   //find out how to add wires
 
